03aa9f4ef9dc65fd32597736b19f6e51d19c2fea
Hackerman <sphincone@gmail.com>
2020-05-02 01:33:05 +0600

    stylesheet and images and titles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,19 +18,8 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Pizzahouse!<br>
-            Fahim's Best
-        </div>
-
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+            <img src="/img/pizza-house.png" alt="Pizza house logo" style="width:20%;height:20%">
+            <br>Fahim's Best Pizza
         </div>
     </div>
 </div>
