1573b8f7d0eaeb2ab5696ac7e39566fa364b989c
Hackerman <sphincone@gmail.com>
2020-05-03 02:32:15 +0600

    Intro to migration and databases

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 });
 
 Route::get('/pizzas', 'PizzaController@index');
-Route::get('/pizzas/{id}', 'PizzaController@show' );
+Route::get('/pizzas/{id}', 'PizzaController@show');
